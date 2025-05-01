d901c2c9a6ddc56062756d022668dc7f02e4dc23
Thanathep Thaithae <justminemind@gmail.com>
2025-05-02 02:54:08 +0800

    feat: Draggable List with dragging animation

--- components/TaskListCard.tsx
+++ components/TaskListCard.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { motion, Reorder, useDragControls } from "motion/react";
+import { motion, Reorder } from "motion/react";
 import { useState } from "react";
 import { FaCheck } from "react-icons/fa";
+import { PiDotsSixVerticalBold } from "react-icons/pi";
 import { v4 as uuid } from "uuid";
 
 type Task = {
@@ -15,8 +16,6 @@ type Task = {
 const TaskListCard = () => {
   const [taskList, setTaskList] = useState<Task[]>([]);
   const [newTask, setNewTask] = useState("");
-
-  const controls = useDragControls();
 
   const addTask = (task: string) => {
     setTaskList((prevState) => [
@@ -52,34 +51,56 @@ const TaskListCard = () => {
       }}
     >
       <motion.div id='clockin' className='flex flex-col'>
-        <h1 className='text-xl font-bold font-serif'> Today's task </h1>
-        <motion.div className='flex flex-col gap-2 mb-4'>
+        <motion.div
+          layout='position'
+          className='flex flex-row gap-2 items-center justify-between'
+        >
+          <h1 className='text-xl font-bold font-serif'> Today's task </h1>
+          <motion.button
+            className='block w-fit rounded-xl  px-3 py-3 text-center text-sm font-semibold text-white '
+            style={{ backgroundColor: "#DDAD4D" }}
+            whileHover={{ backgroundColor: "#C59B2C" }}
+            whileTap={{ backgroundColor: "#C59B2C", scale: 0.98 }}
+            onClick={() => setTaskList([])}
+          >
+            Clear
+          </motion.button>
+        </motion.div>
+        <motion.div className='flex flex-col gap-2 mb-2 mt-4'>
           <Reorder.Group axis='y' values={taskList} onReorder={setTaskList}>
             {taskList.map((task) => (
               <Reorder.Item
                 key={task.id}
                 value={task}
-                drag='y'
-                dragControls={controls}
-                dragListener={false}
                 style={{
-                  backgroundColor: "#fcf3cf",
+                  backgroundColor: "#fafafa",
                   borderRadius: "0.6rem",
-                  padding: "0.5rem",
+                  paddingTop: "0.5rem",
+                  paddingBottom: "0.5rem",
+                  paddingLeft: "0.5rem",
+                  paddingRight: "1rem",
                   touchAction: "none",
+                  marginBottom: "0.6rem",
+                }}
+                whileHover={{
+                  boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.08)",
+                }}
+                whileDrag={{
+                  scale: 1.05,
+                  boxShadow: "0px 0px 30px rgba(0, 0, 0, 0.06)",
                 }}
                 className='flex flex-row gap-2 items-center'
-                onPointerDown={(e) => controls.start(e)}
               >
                 <motion.div className='flex flex-row w-full items-center px-2 py-2'>
                   <motion.button
                     id='checkbox'
                     style={{
-                      marginRight: "0.5rem",
+                      marginRight: "1rem",
                       backgroundColor: "#ffffff",
                       height: "1.5rem",
                       width: "1.5rem",
                       borderRadius: "0.3rem",
+                      border: "1px solid #dddddd",
                     }}
                     className='flex items-center justify-center'
                     onClick={() => toggleTask(task)}
@@ -93,14 +114,20 @@ const TaskListCard = () => {
                     )}
                   </motion.button>
 
-                  <h1 className='text-md font-medium'> {task.task} </h1>
+                  <h1 className='text-md font-medium select-none font-sans'>
+                    {task.task}
+                  </h1>
+                </motion.div>
+                <motion.div className='cursor-move'>
+                  <PiDotsSixVerticalBold scale={1.4} />
                 </motion.div>
               </Reorder.Item>
             ))}
           </Reorder.Group>
         </motion.div>
         <motion.input
-          className='block w-full rounded-xl  px-3 py-4 text-left text-sm font-semibold text-white '
+          layout='position'
+          className='block w-full rounded-xl px-3 py-4 text-left text-sm font-semibold text-white'
           style={{ backgroundColor: "#DDAD4D" }}
           whileHover={{ backgroundColor: "#C59B2C" }}
           whileTap={{ backgroundColor: "#C59B2C", scale: 0.98 }}
