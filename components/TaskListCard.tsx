d9b18256539cbae785c09e737f7c3127131d60c4
Thanathep Thaithae <justminemind@gmail.com>
2025-05-04 14:00:59 +0800

    chore: cleanup add/del task animation

--- components/TaskListCard.tsx
+++ components/TaskListCard.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { motion, Reorder } from "motion/react";
-import { useState } from "react";
+import { AnimatePresence, motion, Reorder } from "motion/react";
+import { useEffect, useState } from "react";
 import { FaCheck } from "react-icons/fa";
-import { PiDotsSixVerticalBold } from "react-icons/pi";
+import { FaTrash } from "react-icons/fa6";
 import { v4 as uuid } from "uuid";
 
 type Task = {
@@ -16,6 +16,13 @@ type Task = {
 const TaskListCard = () => {
   const [taskList, setTaskList] = useState<Task[]>([]);
   const [newTask, setNewTask] = useState("");
+  const [taskLength, setTaskLength] = useState(0);
+  const [taskCompleted, setTaskCompleted] = useState(0);
+
+  useEffect(() => {
+    setTaskLength(taskList.length);
+    setTaskCompleted(taskList.filter((t) => t.completed).length);
+  }, [taskList]);
 
   const addTask = (task: string) => {
     setTaskList((prevState) => [
@@ -27,12 +34,10 @@ const TaskListCard = () => {
         createdAt: new Date().toISOString(),
       },
     ]);
-    console.log(taskList);
   };
 
   const removeTask = (task: Task) => {
     setTaskList((prevState) => prevState.filter((t) => t.id !== task.id));
-    console.log(taskList);
   };
 
   const toggleTask = (task: Task) => {
@@ -44,85 +49,108 @@ const TaskListCard = () => {
   };
 
   return (
-    <motion.div
-      layout
-      style={{
-        padding: "1rem",
-      }}
-    >
-      <motion.div id='clockin' className='flex flex-col'>
+    <motion.div style={{ padding: "1rem" }}>
+      <motion.div layout id='clockin' className='flex flex-col '>
         <motion.div
           layout='position'
           className='flex flex-row gap-2 items-center justify-between'
         >
           <h1 className='text-xl font-bold font-serif'> Today's task </h1>
-          <motion.button
-            className='block w-fit rounded-xl  px-3 py-3 text-center text-sm font-semibold text-white '
-            style={{ backgroundColor: "#DDAD4D" }}
-            whileHover={{ backgroundColor: "#C59B2C" }}
-            whileTap={{ backgroundColor: "#C59B2C", scale: 0.98 }}
-            onClick={() => setTaskList([])}
+          <motion.div
+            animate={{ opacity: taskLength === 0 ? 0 : 1 }}
+            className='inline-flex items-center gap-1 text-md font-medium font-sans'
           >
-            Clear
-          </motion.button>
+            <motion.span initial={{ y: 20 }} animate={{ y: 0 }}>
+              {taskCompleted}
+            </motion.span>
+            /
+            <motion.span initial={{ y: 20 }} animate={{ y: 0 }}>
+              {taskLength}
+            </motion.span>{" "}
+            completed
+          </motion.div>
         </motion.div>
-        <motion.div className='flex flex-col gap-2 mb-2 mt-4'>
+        <motion.div className='flex flex-col gap-2 mb-2 mt-4 relative'>
           <Reorder.Group axis='y' values={taskList} onReorder={setTaskList}>
-            {taskList.map((task) => (
-              <Reorder.Item
-                key={task.id}
-                value={task}
-                style={{
-                  backgroundColor: "#fafafa",
-                  borderRadius: "0.6rem",
-                  paddingTop: "0.5rem",
-                  paddingBottom: "0.5rem",
-                  paddingLeft: "0.5rem",
-                  paddingRight: "1rem",
-                  touchAction: "none",
-                  marginBottom: "0.6rem",
-                }}
-                whileHover={{
-                  boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.08)",
-                }}
-                whileDrag={{
-                  scale: 1.05,
-                  boxShadow: "0px 0px 30px rgba(0, 0, 0, 0.06)",
-                }}
-                className='flex flex-row gap-2 items-center'
-              >
-                <motion.div className='flex flex-row w-full items-center px-2 py-2'>
-                  <motion.button
-                    id='checkbox'
-                    style={{
-                      marginRight: "1rem",
-                      backgroundColor: "#ffffff",
-                      height: "1.5rem",
-                      width: "1.5rem",
-                      borderRadius: "0.3rem",
-                      border: "1px solid #dddddd",
-                    }}
-                    className='flex items-center justify-center'
-                    onClick={() => toggleTask(task)}
-                    whileHover={{ borderColor: "#C59B2C" }}
-                    whileTap={{ scale: 0.98 }}
-                  >
-                    {task.completed ? (
-                      <FaCheck />
-                    ) : (
-                      <FaCheck style={{ color: "#FFFFFF" }} />
-                    )}
-                  </motion.button>
+            <AnimatePresence mode='popLayout'>
+              {taskList.map((task) => (
+                <Reorder.Item
+                  key={task.id}
+                  value={task}
+                  style={{
+                    borderRadius: "0.6rem",
+                    padding: "0.5rem",
+                    touchAction: "none",
+                    marginBottom: "0.6rem",
+                    backgroundColor: "#fafafa",
+                  }}
+                  whileHover={{ boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.08)" }}
+                  whileDrag={{
+                    scale: 1.05,
+                    boxShadow: "0px 0px 30px rgba(0, 0, 0, 0.06)",
+                  }}
+                  initial={{
+                    opacity: 0,
+                    y: 20,
+                    filter: "blur(10px)",
+                  }}
+                  animate={{
+                    opacity: 1,
+                    y: 0,
+                    filter: "blur(0px)",
+                    backgroundColor: "#fafafa",
+                  }}
+                  exit={{
+                    opacity: 0,
+                    x: -20,
+                    filter: "blur(10px)",
+                    transition: { duration: 0.2 },
+                  }}
+                  transition={{ duration: 0.4 }}
+                  layout
+                  className='flex flex-row gap-2 items-center cursor-pointer'
+                >
+                  <motion.div className='flex flex-row w-full items-center px-2 py-2'>
+                    <motion.button
+                      id='checkbox'
+                      style={{
+                        marginRight: "1rem",
+                        backgroundColor: "#ffffff",
+                        height: "1.5rem",
+                        width: "1.5rem",
+                        borderRadius: "0.3rem",
+                        border: "1px solid #dddddd",
+                      }}
+                      className='flex items-center justify-center'
+                      onClick={() => toggleTask(task)}
+                      whileHover={{ borderColor: "#C59B2C" }}
+                      whileTap={{ scale: 0.98 }}
+                    >
+                      {task.completed ? (
+                        <FaCheck />
+                      ) : (
+                        <FaCheck style={{ color: "#FFFFFF" }} />
+                      )}
+                    </motion.button>
 
-                  <h1 className='text-md font-medium select-none font-sans'>
-                    {task.task}
-                  </h1>
-                </motion.div>
-                <motion.div className='cursor-move'>
-                  <PiDotsSixVerticalBold scale={1.4} />
-                </motion.div>
-              </Reorder.Item>
-            ))}
+                    <motion.h1
+                      className='text-md font-medium select-none font-sans'
+                      initial={false}
+                    >
+                      {task.task}
+                    </motion.h1>
+                  </motion.div>
+                  <motion.div
+                    className='cursor-pointer'
+                    style={{ opacity: 0.5 }}
+                    whileHover={{ opacity: 1 }}
+                    onClick={() => removeTask(task)}
+                  >
+                    <FaTrash scale={1.5} />
+                  </motion.div>
+                </Reorder.Item>
+              ))}
+            </AnimatePresence>
           </Reorder.Group>
         </motion.div>
         <motion.input
